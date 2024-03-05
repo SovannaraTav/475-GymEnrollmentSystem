916fb7b8b2c0b74b7c8c1f874f22d5e01767eb27
Adam Chhor <chhoradam@gmail.com>
2024-03-05 14:22:18 -0800

    created template code

--- GymEnrollmentSystem.java
+++ GymEnrollmentSystem.java
@@ -10,12 +10,15 @@ Team 475 - Gym Enrollment System
 import java.sql.*; // Imports all packages tied to the PostgreSQL JDBC Driver
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Properties;
 
 public class GymEnrollmentSystem
 {
+    static Connection conn;
     public static void main(String[] args)
     {
+
         // Retrieves the PostgreSQL database properties
         Properties properties = new Properties();
         String fileName = "DatabaseProperties.properties";
@@ -37,45 +40,14 @@ public class GymEnrollmentSystem
         // Establishes the connection between JDBC and PostgreSQL database
         try
         {
-            Connection databaseConnection =
+            conn =
                     DriverManager.getConnection(url, user, password);
             System.out.println("Database connection established");
+            //PRINT OUT TEST QUERIES HERE:
+//           System.out.println(getMembersInClass("CLS001", 4));
 
-            /*
-             *
-             * SQL query execution example
-             *
-             */
-            PreparedStatement testQuery =
-                    databaseConnection.prepareStatement("SELECT * FROM Employee");
-            ResultSet testQueryResults = testQuery.executeQuery();
-            ResultSetMetaData metaData = testQueryResults.getMetaData();
 
-            // PostgreSQL column indexes start at 1 rather than 0
-            for(int i = 1; i <= metaData.getColumnCount(); i++)
-            {
-                System.out.printf("%-20s", metaData.getColumnName(i));
-            }
-            System.out.println();
-
-            while (testQueryResults.next())
-            {
-                for(int i = 1; i <= metaData.getColumnCount(); i++)
-                {
-                    System.out.printf("%-20s", testQueryResults.getString(i));
-                }
-                System.out.println();
-            }
-
-            testQueryResults.close();
-            testQuery.close();
-            /*
-             *
-             *
-             *
-             */
-
-            databaseConnection.close();
+            conn.close();
             System.out.println("Database connection closed");
         }
         catch (SQLException e)
@@ -83,4 +55,485 @@ public class GymEnrollmentSystem
             System.err.println("Database connection error: " + e.getMessage());
         }
     }
+
+    /** #1
+     * @Author Sandesh Rai
+     * @return returns true if
+     */
+    public static boolean addInstructorToClass(String classNumber, String instructorNumber) {
+        try {
+        PreparedStatement query = conn.prepareStatement("""
+                
+                """
+        );
+        ResultSet result = query.executeQuery();
+        } catch(SQLException e) {
+            return false;
+        }
+        return true;
+    }
+    /** #2
+     * @Author Sandesh Rai
+     * @return returns true if
+     */
+    public static boolean updateInstructorInClass(String classNumber, String newInstructorNumber) {
+        try {
+            PreparedStatement query = conn.prepareStatement("""
+                
+                """
+            );
+            ResultSet result = query.executeQuery();
+        } catch(SQLException e) {
+            return false;
+        }
+        return true;
+    }
+    /** #3
+     * @Author Sandesh Rai
+     * @return returns true if
+     */
+    public static boolean removeInstructorFromClass(String classNumber, String instructorNumber) {
+        try {
+            PreparedStatement query = conn.prepareStatement("""
+                
+                """
+            );
+            ResultSet result = query.executeQuery();
+        } catch(SQLException e) {
+            return false;
+        }
+        return true;
+    }
+    /** #4
+     * @Author Sandesh Rai
+     * @return returns true if
+     */
+    public static boolean addMemberToClass(String classNumber, String memberNumber) {
+        try {
+            PreparedStatement query = conn.prepareStatement("""
+                
+                """
+            );
+            ResultSet result = query.executeQuery();
+        } catch(SQLException e) {
+            return false;
+        }
+        return true;
+    }
+    /** #5
+     * @Author Sandesh Rai
+     * @return returns true if
+     */
+    public static boolean updateMemberClassEnrollment(String oldClassNumber, String newClassNumber, String memberNumber) {
+        try {
+            PreparedStatement query = conn.prepareStatement("""
+                
+                """
+            );
+            ResultSet result = query.executeQuery();
+        } catch(SQLException e) {
+            return false;
+        }
+        return true;
+    }
+    /** #6
+     * @Author Sandesh Rai
+     * @return returns true if
+     */
+    public static boolean removeMemberFromClass(String classNumber, String memberNumber) {
+        try {
+            PreparedStatement query = conn.prepareStatement("""
+                
+                """
+            );
+            ResultSet result = query.executeQuery();
+        } catch(SQLException e) {
+            return false;
+        }
+        return true;
+    }
+    /** #7
+     * @Author Sandesh Rai
+     * @return returns true if
+     */
+    public static boolean addMember(String firstName, String lastName, String email, String phoneNumber, String memberNumber) {
+        try {
+            PreparedStatement query = conn.prepareStatement("""
+                
+                """
+            );
+            ResultSet result = query.executeQuery();
+        } catch(SQLException e) {
+            return false;
+        }
+        return true;
+    }
+    /** #8
+     * @Author Sandesh Rai
+     * @return returns true if
+     */
+    public static boolean updateMemberEmail(String memberNumber, String newValue) {
+        try {
+            PreparedStatement query = conn.prepareStatement("""
+                
+                """
+            );
+            ResultSet result = query.executeQuery();
+        } catch(SQLException e) {
+            return false;
+        }
+        return true;
+    }
+    /** #9
+     * @Author Sandesh Rai
+     * @return returns true if
+     */
+    public static boolean updateMemberPhoneNumber(String memberNumber, String newValue) {
+        try {
+            PreparedStatement query = conn.prepareStatement("""
+                
+                """
+            );
+            ResultSet result = query.executeQuery();
+        } catch(SQLException e) {
+            return false;
+        }
+        return true;
+    }
+    /** #10
+     * @Author Sandesh Rai
+     * @return returns true if
+     */
+    public static boolean updateMemberLastPayment(String memberNumber, String newValue) {
+        try {
+            PreparedStatement query = conn.prepareStatement("""
+                
+                """
+            );
+            ResultSet result = query.executeQuery();
+        } catch(SQLException e) {
+            return false;
+        }
+        return true;
+    }
+    /** #11
+     * @Author Sandesh Rai
+     * @return returns true if
+     */
+    public static boolean removeMember(String memberNumber) {
+        try {
+            PreparedStatement query = conn.prepareStatement("""
+                
+                """
+            );
+            ResultSet result = query.executeQuery();
+        } catch(SQLException e) {
+            return false;
+        }
+        return true;
+    }
+    /** #12
+     * @Author Sovannara Tav
+     * @return returns true if
+     */
+    public static boolean addInstructor(String firstName, String lastName, String email, String phoneNumber, String instructorNumber)
+    {
+        try {
+            PreparedStatement query = conn.prepareStatement("""
+                
+                """
+            );
+            ResultSet result = query.executeQuery();
+        } catch(SQLException e) {
+            return false;
+        }
+        return true;
+    }
+    /** #13
+     * @Author Sovannara Tav
+     * @return returns true if
+     */
+    public static boolean updateInstructorEmail(String instructorNumber, String newValue)
+    {
+        try {
+            PreparedStatement query = conn.prepareStatement("""
+                
+                """
+            );
+            ResultSet result = query.executeQuery();
+        } catch(SQLException e) {
+            return false;
+        }
+        return true;
+    }
+    /** #14
+     * @Author Sovannara Tav
+     * @return returns true if
+     */
+    public static boolean updateInstructorphoneNumber(String instructorNumber, String newValue)
+    {
+        try {
+            PreparedStatement query = conn.prepareStatement("""
+                
+                """
+            );
+            ResultSet result = query.executeQuery();
+        } catch(SQLException e) {
+            return false;
+        }
+        return true;
+    }
+    /** #15
+     * @Author Sovannara Tav
+     * @return returns true if
+     */
+    public static boolean removeInstrutor(String instructorNumber)
+    {
+        try {
+            PreparedStatement query = conn.prepareStatement("""
+                
+                """
+            );
+            ResultSet result = query.executeQuery();
+        } catch(SQLException e) {
+            return false;
+        }
+        return true;
+    }
+    /** #16
+     * @Author Sovannara Tav
+     * @return returns true if
+     */
+    public static boolean addClass(String classTypeName, String instructorNumber, String roomNumber, String startTime, String duration, String classNumber)
+    {
+        try {
+            PreparedStatement query = conn.prepareStatement("""
+                
+                """
+            );
+            ResultSet result = query.executeQuery();
+        } catch(SQLException e) {
+            return false;
+        }
+        return true;
+    }
+    /** #17
+     * @Author Sovannara Tav
+     * @return returns true if
+     */
+    public static boolean updateClassStartTIme(String classNumber, String newValue)
+    {
+        try {
+            PreparedStatement query = conn.prepareStatement("""
+                
+                """
+            );
+            ResultSet result = query.executeQuery();
+        } catch(SQLException e) {
+            return false;
+        }
+        return true;
+    }
+    /** #18
+     * @Author Sovannara Tav
+     * @return returns true if
+     */
+    public static boolean updateClassDuration(String classNumber, String newValue)
+    {
+        try {
+            PreparedStatement query = conn.prepareStatement("""
+                
+                """
+            );
+            ResultSet result = query.executeQuery();
+        } catch(SQLException e) {
+            return false;
+        }
+        return true;
+    }
+    /** #19
+     * @Author Sovannara Tav
+     * @return returns true if
+     */
+    public static boolean updateClassRoomNumber(String classNumber, String newValue)
+    {
+        try {
+            PreparedStatement query = conn.prepareStatement("""
+                
+                """
+            );
+            ResultSet result = query.executeQuery();
+        } catch(SQLException e) {
+            return false;
+        }
+        return true;
+    }
+    /** #20
+     * @Author Sovannara Tav
+     * @return returns true if
+     */
+    public static boolean removeClass(String classNumber)
+    {
+        try {
+            PreparedStatement query = conn.prepareStatement("""
+                
+                """
+            );
+            ResultSet result = query.executeQuery();
+        } catch(SQLException e) {
+            return false;
+        }
+        return true;
+    }
+    /** #21
+     * @Author Sovannara Tav
+     * @return returns true if
+     */
+    public static boolean addRoom(String roomNumber, int capacity)
+    {
+        try {
+            PreparedStatement query = conn.prepareStatement("""
+                
+                """
+            );
+            ResultSet result = query.executeQuery();
+        } catch(SQLException e) {
+            return false;
+        }
+        return true;
+    }
+    /** #22
+     * @Author Sovannara Tav
+     * @return returns true if
+     */
+    public static boolean updateRoomNumber(String roomNumber, String newValue) // Ask professor on this???
+    {
+        try {
+            PreparedStatement query = conn.prepareStatement("""
+                
+                """
+            );
+            ResultSet result = query.executeQuery();
+        } catch(SQLException e) {
+            return false;
+        }
+        return true;
+    }
+    /** #23
+     * @Author Sovannara Tav
+     * @return returns true if
+     */
+    public static boolean updateRoomCapacity(String roomNumber, int newValue) // Ask professor on this)
+    {
+        try {
+            PreparedStatement query = conn.prepareStatement("""
+                
+                """
+            );
+            ResultSet result = query.executeQuery();
+        } catch(SQLException e) {
+            return false;
+        }
+        return true;
+    }
+    /** #24
+     * @Author Sovannara Tav
+     * @return returns true if
+     */
+    public static boolean removeRoom(String roomNumber)
+    {
+        try {
+            PreparedStatement query = conn.prepareStatement("""
+                
+                """
+            );
+            ResultSet result = query.executeQuery();
+        } catch(SQLException e) {
+            return false;
+        }
+        return true;
+    }
+    /** #25
+     * @Author Sovannara Tav
+     * @return returns true if
+     */
+    public static boolean addClassType(String name)
+    {
+        try {
+            PreparedStatement query = conn.prepareStatement("""
+                
+                """
+            );
+            ResultSet result = query.executeQuery();
+        } catch(SQLException e) {
+            return false;
+        }
+        return true;
+    }
+    /** #26
+     * @Author Sovannara Tav
+     * @return returns true if
+     */
+    public static boolean removeClassType(String name)
+    {
+        try {
+            PreparedStatement query = conn.prepareStatement("""
+                
+                """
+            );
+            ResultSet result = query.executeQuery();
+        } catch(SQLException e) {
+            return false;
+        }
+        return true;
+    }
+    /** #27
+     * @Author Sovannara Tav
+     * @return returns true if
+     */
+    public static boolean checkGymMembership(String memberNumber)
+    {
+        try {
+            PreparedStatement query = conn.prepareStatement("""
+                
+                """
+            );
+            ResultSet result = query.executeQuery();
+        } catch(SQLException e) {
+            return false;
+        }
+        return true;
+    }
+    /** #28
+     * @Author Adam Chhor
+     * @return Formatted String Table of
+     */
+    public static String getInstructorForClass(String classNumber) throws SQLException {
+        PreparedStatement query = conn.prepareStatement("""
+                
+                """
+        );
+        ResultSet result = query.executeQuery();
+        return Util.parseDataBase(result);
+    }
+    /** #29
+     * @Author Adam Chhor
+     * @return Formatted String Table of members' information of "classNumber" and limits column by "numberOfRowsToReturn"
+     */
+    public static String getMembersInClass(String classNumber, int numberOfRowsToReturn) throws SQLException {
+        PreparedStatement query = conn.prepareStatement("""
+                SELECT firstName, lastName, email, phoneNumber, startDate, memberNumber
+                FROM Member M
+                    JOIN Participants P ON P.memberId = M.id
+                    JOIN Class C ON C.id = P.classId
+                WHERE C.classNumber = ?
+                LIMIT ?
+                """
+        );
+        query.setString(1, classNumber);
+        query.setInt(2, numberOfRowsToReturn);
+        ResultSet result = query.executeQuery();
+        return Util.parseDataBase(result);
+    }
+
 }
---
+++ Util.java
@@ -0,0 +1,27 @@
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
+public class Util {
+  public static String parseDataBase(ResultSet rs) throws SQLException {
+    StringBuilder formattedStringBuilder = new StringBuilder();
+    ResultSetMetaData metaData = rs.getMetaData();
+    for(int i = 1; i <= metaData.getColumnCount(); i++)
+    {
+      formattedStringBuilder.append(String.format("%-20s", metaData.getColumnName(i)));
+    }
+    formattedStringBuilder.append("\n");
+    formattedStringBuilder.append("-".repeat(20* metaData.getColumnCount()));
+    formattedStringBuilder.append("\n");
+    while (rs.next())
+    {
+      for(int i = 1; i <= metaData.getColumnCount(); i++)
+      {
+        formattedStringBuilder.append(String.format("%-20s", rs.getString(i)));
+      }
+      formattedStringBuilder.append("\n");
+    }
+    return formattedStringBuilder.toString();
+  }
+}
